7c624af05b8e2255aa4732e4d5f8945a008ca13e
Grrom <josejeromelalunio@gmail.com>
2023-01-13 11:18:18 +0800

    add login

--- app/src/main/java/com/example/shogo/LoginActivity.java
+++ app/src/main/java/com/example/shogo/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.shogo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,8 +16,23 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button loginButton = findViewById(R.id.login_button);
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
 
         loginButton.setOnClickListener(view -> {
+            if(username.getText().toString().isEmpty() || password.getText().toString().isEmpty()){
+                Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if(!username.getText().toString().equals("gab")){
+                Toast.makeText(this, "User not found.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if(!password.getText().toString().equals("gab")){
+                Toast.makeText(this, "Incorrect password.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
             startActivity(new Intent(this, MainActivity.class));
         });
     }
